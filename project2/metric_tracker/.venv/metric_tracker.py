171154d292107c5c2c5bc369806cc2b4f46e4cb1
CameronHunter614 <cameronhunter@sbcglobal.net>
2024-03-25 09:15:34 -0700

    python write to csv fixed

--- project2/metric_tracker/.venv/metric_tracker.py
+++ project2/metric_tracker/.venv/metric_tracker.py
@@ -5,7 +5,7 @@ import csv
 
 def writeToCSV(filename : str, metrics: dict):
     with open(file=filename, mode="w", newline="") as fp:
-        writer = csv.Dictwriter(fp, fieldnames=metrics[0].keys())
+        writer = csv.DictWriter(fp, fieldnames=metrics[0].keys())
 
         # Write header row
         writer.writeheader()
@@ -22,10 +22,9 @@ def main():
     driver.get("http://localhost:3000/")
 
     # Initialize variables
-    fields = ['Timestamp', 'Action Type', 'Value']
-    metrics = {} # {Presence Time (seconds) or Scrolling (Pixels)}
+    metrics = [] # {Presence Time (seconds) or Scrolling (Pixels)}
     filename = "metrics.csv"
-    sample_size = 10
+    sample_size = 2
     count = 0
     start_time = time.time()
 
@@ -34,13 +33,16 @@ def main():
         current_time = time.time()
         presence_time = current_time - start_time
         print(f"Presence time: {presence_time} seconds")
-        metrics.append([current_time, 'Presence', presence_time])
+        #metrics["Presence time (Seconds)"].append(presence_time)
         
         # Track scrolling
         scroll_height = driver.execute_script("return document.body.scrollHeight")  
         current_scroll = driver.execute_script("return window.pageYOffset")
         print(f"Scrolled {current_scroll}/{scroll_height} pixels")
-        metrics.append([current_time, 'Scroll', current_scroll/scroll_height])
+        #metrics["Scrolling (Pixels)"].append(current_scroll/scroll_height)
+        metrics.append({"TIMESTAMP (HH:MM:SS)": time.strftime("%H:%M:%S", time.localtime()), 
+                        "Presence Time (Seconds)": presence_time,
+                        "Scrolling (Pixels)": current_scroll/scroll_height})
         
         count += 1
         time.sleep(2) 
